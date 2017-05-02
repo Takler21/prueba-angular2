8ef3010053ca011fe319c9487b0931d9a9540a03
Takler21 <ignaciojazpiroz@gmail.com>
2017-05-02 13:59:28 +0200

    Delete post.component.ts

--- src/app/post.component.ts
+++
@@ -1,3 +0,0 @@
-﻿import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Post, GroupPosts } from './post.interface';
