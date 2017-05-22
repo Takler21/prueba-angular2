35a0c1ad9f16854f9aca7a182a0369f18fb9a0f2
I.A <Takler21@users.noreply.github.com>
2017-05-22 15:46:24 +0200

    Add files via upload

---
+++ ConservarV3.0/collapse.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { NgFor, NgIf }         from '@angular/common';
+@Component({
+  selector: 'my-collap',
+  templateUrl: 'app/collapse.component.html'
+})
+export class CollapseComponent implements OnInit {
+  @Input() nombre: string;
+  @Input() id: string;
+  @Input() ref: string;
+  @Input() isOpen = true;
+  @Output() opened = new EventEmitter();
+  @Output() closed = new EventEmitter();
+
+
+ngOnInit() {
+    if (this.id)
+    {
+        this.ref = "index.html#" + this.id;
+    }
+}
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+    this.isOpen ? this.opened.emit() : this.closed.emit();
+  }
+}
