6bc100873f0d45a08e50614284d12ea6ebecc2f5
I.A <Takler21@users.noreply.github.com>
2017-06-09 14:50:00 +0200

    Delete app.settings.ts

--- con calendar/app.settings.ts
+++
@@ -1,6 +0,0 @@
-﻿export class AppSettings {
-    public static DATA2 = "http://localhost:3000/";
-    public static DATA3 = "http://localhost:4000/";
-    public static dateFormat1 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
-    public static dateFormat2 = /^\d{4}-\d{2}-\d{2}/;
-}
